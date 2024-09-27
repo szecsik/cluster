d3a83a2643c5ec01ede40f05da95abdcf81def63
Krisztian <szecsikr@gmail.com>
2024-09-27 16:02:02 +0200

    backend swap

--- provider.tf
+++ provider.tf
@@ -1,12 +1,5 @@
 terraform {
 
- backend "azurerm" {
-    resource_group_name  = "szecsikr" 
-    storage_account_name = "szecsikr"                
-    container_name       = "tfstate"               
-    key                  = "szecsikr-cluster.terraform.tfstate"
-  }
-  
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -16,13 +9,20 @@ terraform {
 
 }
 
-
-
-
-
 provider "azurerm" {
   #resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
   subscription_id = "5610ba8d-b1da-49de-87f9-b014a6efe236"
   use_msi = true
+}
+
+terraform {
+
+ backend "azurerm" {
+    resource_group_name  = "szecsikr" 
+    storage_account_name = "szecsikr"                
+    container_name       = "tfstate"               
+    key                  = "szecsikr-cluster.terraform.tfstate"
+  }
+  
 }
