b6d0bd22d3e28ab6ebd8eb98e6317a82d1ecd014
Krisztian <szecsikr@gmail.com>
2024-09-27 14:30:20 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "example-aks1"
+  location            = "Germany West Central"
+  resource_group_name = "szecsikr"
+  dns_prefix          = "szecsikr"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.example.kube_config[0].client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+
+  sensitive = true
+}
+
---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
+provider "azurerm" {
+  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+  subscription_id = "5610ba8d-b1da-49de-87f9-b014a6efe236"
+}
